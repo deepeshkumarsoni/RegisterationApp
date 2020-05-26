4fa8379cd900616339246d36b2dcd70187c49cdb
Deepesh <deepesh.kt10636@gmail.com>
2020-05-26 17:20:36 +0530

    modify

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Time } from '@angular/common';
 
 export interface User{
   firstName : string;
@@ -10,8 +11,15 @@ export interface User{
   country :string;
   state : string;
   city : string;
-  dob : number;  
-  academic : string;
+  dateOfBirth : Date;  
+  timeOfBirth : string; 
+  academyList : string[];
+};
+
+export interface Academy {
+  name: string;
+  id: string;
+  selected: boolean;
 }
 
 @Component({
@@ -19,30 +27,57 @@ export interface User{
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'RegisterationApp';
   user : User;
   userList : User[];
-  
+  academyList : Academy[] = [];
+
   constructor(){
      this.user = {
-        firstName:'',
-        lastName:'',
-        age : 0,
-        gender : '',
-        email : '',
-        password : '',
-        country : '',
-        state : '',
-        city : '',
-        dob : 0,
-        academic : '',
+        firstName:'Deepesh',
+        lastName:'Soni',
+        age : 28,
+        gender : 'M',
+        email : 'deepesh@gmail.com',
+        password : 'abcd',
+        country : 'India',
+        state : 'CG',
+        city : 'Bhilai',
+        dateOfBirth : new Date('14/3/1991') ,
+        timeOfBirth : '2 PM',
+        academyList : ['1','2']
       };
-      this.userList = [];
+    
+      this.academyList.push(
+        {       
+          name : "10th",
+          id : '1',
+          selected : false,
+        },
+        {
+          name : "12th",
+          id : '2',
+          selected : false,
+        },
+        { 
+          name : "Graduate",
+          id : '3',
+          selected : true,
+        }
+      );  
+        this.userList = [];
   }
+   
+    isSelected(id: string) {
+      return this.user.academyList.indexOf(id) > -1;
+    }
+  
+} 
 
-  register(){
+  
+  
 
-  }
+  
 
-}
