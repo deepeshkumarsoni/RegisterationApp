4237210c5b3d24c40db1fa088922ea558e70b85c
Deepesh <deepesh.kt10636@gmail.com>
2020-06-08 18:40:13 +0530

    ngModule error solve

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';  
+import {FormsModule} from '@angular/forms';
+import { NgModule } from '@angular/core';  
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserRegisterationComponent } from './user-registeration/user-registeration.component';
